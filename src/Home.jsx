97d39bf44ee7dd0e0c47a77bbc3a78cb0dd812c0
Kishore <your-email@example.com>
2025-03-05 14:11:31 +0530

    added react

---
+++ src/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h2>Kishore</h2>
+            <p>Contact: kishore@example.com</p>
+        </div>
+    );
+};
+
+export default Home;
